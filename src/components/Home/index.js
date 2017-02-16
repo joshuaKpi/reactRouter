01a214b2bb64c24e4f005dff57aa372180c4efeb
joshuaKpi <joshua.kpi@gmail.com>
2017-02-16 16:36:53 +0200

    Programmable routing

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const value = e.target.elements[0].value.toLowerCase();
+    this.context.router.push(`/genre/${value}`);
+  }
+
   render() {
     return (
       <div className='row'>
         <div className='col-md-12'>Раздел /home</div>
+        <form className='col-md-4' onSubmit={this.handleSubmit}>
+          <input type='text' placeholder='genreName'/>
+          <button type='submit'>Go</button>
+        </form>
       </div>
     )
   }
 }
+
+Home.contextTypes = {
+  router: PropTypes.object.isRequired
+};
