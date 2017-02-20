6f2ae71c046194c004c0c2d1bf6d2ddda1f45418
joshuaKpi <joshua.kpi@gmail.com>
2017-02-20 17:09:41 +0200

    Before redux clean Home.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,39 +1,11 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 export default class Home extends Component {
-  constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    const value = e.target.elements[0].value.toLowerCase();
-    this.context.router.push(`/genre/${value}`);
-  }
-
-  componentDidMount() {
-    this.context.router.setRouteLeaveHook(this.props.route, this.routerWillLeave)
-  }
-
-  routerWillLeave() {
-    let answer = window.confirm('Are You sure?');
-    if (!answer) return false;
-  }
-
   render() {
     return (
       <div className='row'>
-        <div className='col-md-12'>Раздел /home</div>
-        <form className='col-md-4' onSubmit={this.handleSubmit}>
-          <input type='text' placeholder='genreName'/>
-          <button type='submit'>Go</button>
-        </form>
+        <div className='col-md-12'>Before implementing Redux</div>
       </div>
     )
   }
 }
-
-Home.contextTypes = {
-  router: PropTypes.object.isRequired
-};
