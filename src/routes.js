72bed00d9dd9dcf010545e7ad8f4e906ebac3e52
joshuaKpi <joshua.kpi@gmail.com>
2017-02-21 11:40:04 +0200

    Redirect using store.dispatch. Broke onEnter hook

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import Genre from './components/Genre';
 import Home from './components/Home';
 import List from './components/List';
 import Release from './components/Release';
-import Login from './components/Login';
+import LoginPage from './containers/LoginPage';
 import NotFound from './components/NotFound';
 
 export const routes = (
@@ -19,7 +19,7 @@ export const routes = (
         <Route path='/genre/:genre/:release' component={Release} />
       </Route>
       <Route path='list' component={List} />
-      <Route path='login' component={Login} />
+      <Route path='login' component={LoginPage} />
     </Route>
     <Route path='*' component={NotFound} />
   </div>
